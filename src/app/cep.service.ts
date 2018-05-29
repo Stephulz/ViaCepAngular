525238ebf223b50c990c5d7bdcd4d265067b0f71
Stefan Sanches <stefansanches9@gmail.com>
2018-05-29 15:09:01 -0300

    first commit

---
+++ src/app/cep.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http'
+import { promise } from 'protractor';
+import { Cep } from './cep';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CepService {
+
+  constructor(private http:Http) { }
+
+  buscar(cep:string){
+    //this.http.get(`https://viacep.com.br/ws/${cep}/json/`) interpolação de string
+    return this.http.get('https://viacep.com.br/ws/'+cep+'/json/')
+    //this.http.get('https://stfangular.atlassian.net/rest/api/2/project')
+    .toPromise()
+    .then(response => {
+      console.log(response);
+      return this.converterResposParaCep(response.json());
+    });
+  }
+
+  private converterResposParaCep(cepNaResposta):Cep{
+    let cep = new Cep();
+    cep.cep = cepNaResposta.cep;
+    cep.logradouro = cepNaResposta.logradouro;
+    cep.complemento = cepNaResposta.complemento;
+    cep.bairro = cepNaResposta.bairro;
+    cep.cidade = cepNaResposta.cidade;
+    cep.estado = cepNaResposta.uf;
+    return cep;
+  }
+
+}
